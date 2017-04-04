f0cc63e89ceb91caf0a60dd1404835bc25dad730
Arnold Szabo <szaboaarnold@gmail.com>
2017-04-04 21:27:54 +0300

    Add SearchView into MainActivity

--- app/src/main/java/com/cdev/showtracker/MainActivity.kt
+++ app/src/main/java/com/cdev/showtracker/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.cdev.showtracker
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import com.cdev.showtracker.search.SearchActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -14,24 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
         viewPager.adapter = MainPagerAdapter(supportFragmentManager)
         tabLayout.setupWithViewPager(viewPager)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when (item?.itemId) {
-            R.id.option_search -> {
-                startActivity(Intent(this, SearchActivity::class.java))
-                return true
-            }
-            else -> return super.onOptionsItemSelected(item)
-        }
     }
 }
